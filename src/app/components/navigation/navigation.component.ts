aec376396356cdcd7bc0e8ffdbc5232ed152d0de
Josiel Faria <developerjosiel@gmail.com>
2025-05-29 12:33:19 -0300

    fix active nav

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ControlService } from 'src/app/shared/services/control.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {
-  constructor(private router: Router) {}
+export class NavigationComponent implements OnInit {
+  constructor(private router: Router, private controlService: ControlService) {}
 
-  pathFocus: string = 'home';
+  pathFocus: string = '';
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.pathFocus = this.router.url.replace('/', '');
+    });
+  }
 
   nagivateTo(path: string): void {
     this.pathFocus = path;
+
+    if (path !== 'home') {
+      this.controlService.stopMetronome();
+    }
 
     this.router.navigate([path]).then((success) => {
       if (success) {
