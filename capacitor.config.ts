12c157a5bbbb67fbf227f7933262dba9f73a932b
Josiel Faria <developerjosiel@gmail.com>
2025-05-29 16:02:26 -0300

    Add build android apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mytimemyrule.app',
+  appName: 'mytimemyrules',
+  webDir: 'dist'
+};
+
+export default config;
