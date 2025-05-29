380181c2bab83256ed47e6757d0068221d9e8141
Josiel Faria <developerjosiel@gmail.com>
2025-05-29 12:51:13 -0300

    Add configs

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.page.html',
-  styleUrls: ['./config.page.scss']
+  styleUrls: ['./config.page.scss'],
 })
-export class ConfigPage {
+export class ConfigPage implements OnInit {
+  stepSlideRangerBpm: number = 10;
+  stepControlButtonsBpm: number = 10;
 
+  constructor(private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    const storedStepSlideRangerBpm = this.storageService.getItem('slideRangerBpm');
+    const storedStepControlButtonsBpm = this.storageService.getItem('controlButtonsBpm');
+
+    if (storedStepSlideRangerBpm) {
+      this.stepSlideRangerBpm = parseInt(storedStepSlideRangerBpm, 10);
+    }
+
+    if (storedStepControlButtonsBpm) {
+      this.stepControlButtonsBpm = parseInt(storedStepControlButtonsBpm, 10);
+    }
+  }
+
+  selectIncreaseDecreaseSlideRangerBpm(step: number): void {
+    this.stepSlideRangerBpm = step;
+
+    this.storageService.setItem('slideRangerBpm', String(step));
+  }
+
+  selectIncreaseDecreaseControlButtonsBpm(step: number): void {
+    this.stepControlButtonsBpm = step;
+    this.storageService.setItem('controlButtonsBpm', String(step));
+  }
 }
